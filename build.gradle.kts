e71149014a2781d9269781f794a7e8f2524fc7f9
profitsw2000 <profitsw2000@gmail.com>
2024-05-12 21:07:53 +0300

    Создана папка для объявления библиотек проекта и хранения версии библиотеки.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,52 @@
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.api.AndroidBasePlugin
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    kotlin("kapt") version "1.9.24" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+
+    plugins.withType<AndroidBasePlugin> {
+
+        configure<BaseExtension> {
+            compileSdkVersion(Config.compile_sdk)
+
+            buildFeatures.viewBinding = true
+
+            defaultConfig {
+                minSdkVersion(Config.min_sdk)
+                targetSdkVersion(Config.target_sdk)
+                versionCode(Releases.version_code)
+                versionName(Releases.version_name)
+                testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+            }
+
+            compileOptions {
+                sourceCompatibility(Config.java_version)
+                targetCompatibility(Config.java_version)
+            }
+        }
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
