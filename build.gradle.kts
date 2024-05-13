2221a357dda662db20fe002c2bba50038e2897f7
profitsw2000 <profitsw2000@gmail.com>
2024-05-13 16:22:44 +0300

    Удален buildSrc в связи с тем что решено использовать Version Library и Convention Plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,9 @@ import com.android.build.gradle.api.AndroidBasePlugin
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    kotlin("kapt") version "1.9.24" apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
 
 buildscript {
@@ -15,7 +16,7 @@ buildscript {
     }
 }
 
-allprojects {
+/*allprojects {
     repositories {
         google()
         mavenCentral()
@@ -45,7 +46,7 @@ subprojects {
             }
         }
     }
-}
+}*/
 
 task<Delete>("clean") {
     delete(rootProject.buildDir)
