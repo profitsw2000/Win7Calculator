6969e997fbae01a7fe201a1f6bfeac34568bd555
profitsw2000 <profitsw2000@gmail.com>
2024-05-19 22:10:36 +0300

    Строки и размеры убраны в ресурсы.

--- standard/build.gradle.kts
+++ standard/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 
 dependencies {
     implementation(project(":core"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
