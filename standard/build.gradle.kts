34aca19c340f120de9b329321ccf15472f09cb5a
profitsw2000 <profitsw2000@gmail.com>
2024-06-07 14:48:02 +0300

    В модули app и standard добавлена библиотека Koin для внедрения зависимостей.

--- standard/build.gradle.kts
+++ standard/build.gradle.kts
@@ -14,8 +14,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    //ViewModel
     implementation(libs.androidx.livedata)
     implementation(libs.androidx.viewmodel)
+    //Koin
+    implementation(libs.koin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
