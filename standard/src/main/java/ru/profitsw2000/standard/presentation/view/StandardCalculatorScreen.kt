bfde81a3af5b44af541df3af965b5c28b8118a26
profitsw2000 <profitsw2000@gmail.com>
2024-06-07 16:50:47 +0300

    Во фрагменте обычного калькулятора сделана инициализация всех кнопок - созданы слушатели кнопок.

--- standard/src/main/java/ru/profitsw2000/standard/presentation/view/StandardCalculatorScreen.kt
+++ standard/src/main/java/ru/profitsw2000/standard/presentation/view/StandardCalculatorScreen.kt
@@ -5,9 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.profitsw2000.data.BUTTON_ADD_CODE
+import ru.profitsw2000.data.BUTTON_BACKSPACE_CODE
+import ru.profitsw2000.data.BUTTON_CLEAR_ALL_CODE
+import ru.profitsw2000.data.BUTTON_CLEAR_ENTERED_CODE
+import ru.profitsw2000.data.BUTTON_DIVIDE_CODE
+import ru.profitsw2000.data.BUTTON_EQUAL_CODE
+import ru.profitsw2000.data.BUTTON_MEMORY_ADD_CODE
+import ru.profitsw2000.data.BUTTON_MEMORY_CLEAR_CODE
+import ru.profitsw2000.data.BUTTON_MEMORY_READ_CODE
+import ru.profitsw2000.data.BUTTON_MEMORY_SAVE_CODE
+import ru.profitsw2000.data.BUTTON_MEMORY_SUBTRACT_CODE
+import ru.profitsw2000.data.BUTTON_MULTIPLY_CODE
+import ru.profitsw2000.data.BUTTON_PERCENTAGE_CODE
+import ru.profitsw2000.data.BUTTON_PLUS_MINUS_CODE
+import ru.profitsw2000.data.BUTTON_RECIPROC_CODE
+import ru.profitsw2000.data.BUTTON_SQUARE_ROOT_CODE
+import ru.profitsw2000.data.BUTTON_SUBTRACT_CODE
 import ru.profitsw2000.standard.R
+import ru.profitsw2000.standard.databinding.FragmentStandardCalculatorScreenBinding
+import ru.profitsw2000.standard.presentation.viewmodel.StandardCalculatorViewModel
 
 class StandardCalculatorScreen : Fragment() {
+
+    private var _binding: FragmentStandardCalculatorScreenBinding? = null
+    private val binding get() = _binding!!
+    private val standardCalculatorViewModel: StandardCalculatorViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +40,102 @@ class StandardCalculatorScreen : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_standard_calculator_screen, container, false)
+        _binding = FragmentStandardCalculatorScreenBinding.bind(inflater.inflate(R.layout.fragment_standard_calculator_screen, container, false))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        initButtons()
+        initMenuTextViews()
+    }
+
+    private fun initMenuTextViews() {
+
+    }
+
+    private fun initButtons() {
+        initMemoryButtons()
+        initClearButtons()
+        initBasicOperationButtons()
+        initSimpleOperationButtons()
+        initDigitButtons()
+    }
+
+    private fun initMemoryButtons() = with(binding) {
+        mcButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MEMORY_CLEAR_CODE) }
+        mrButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MEMORY_READ_CODE) }
+        msButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MEMORY_SAVE_CODE) }
+        mplusButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MEMORY_ADD_CODE) }
+        mminusButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MEMORY_SUBTRACT_CODE) }
+    }
+
+    private fun initClearButtons() = with(binding) {
+        backspaceButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_BACKSPACE_CODE) }
+        ceButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_CLEAR_ENTERED_CODE) }
+        cButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_CLEAR_ALL_CODE) }
+    }
+
+    private fun initBasicOperationButtons() = with(binding) {
+        plusMinusButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_PLUS_MINUS_CODE) }
+        divideButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_DIVIDE_CODE) }
+        multiplyButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_MULTIPLY_CODE) }
+        minusButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_SUBTRACT_CODE) }
+        addButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_ADD_CODE) }
+        equalButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_EQUAL_CODE) }
+    }
+
+    private fun initDigitButtons() = with(binding) {
+        zeroButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.zero_button_text)) }
+        oneButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.one_button_text)) }
+        twoButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.two_button_text)) }
+        threeButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.three_button_text)) }
+        fourButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.four_button_text)) }
+        fiveButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.five_button_text)) }
+        sixButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.six_button_text)) }
+        sevenButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.seven_button_text)) }
+        eightButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.eight_button_text)) }
+        nineButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.nine_button_text)) }
+        pointButton.setOnClickListener { standardCalculatorViewModel.appendDigit(
+            resources.getString(ru.profitsw2000.core.R.string.comma_button_text)) }
+    }
+
+    private fun initSimpleOperationButtons() = with(binding) {
+        sqrtButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_SQUARE_ROOT_CODE) }
+        percentageButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_PERCENTAGE_CODE) }
+        reciprocButton.setOnClickListener { standardCalculatorViewModel.performOperation(
+            BUTTON_RECIPROC_CODE) }
     }
 }
