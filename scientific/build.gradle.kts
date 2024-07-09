aef3348fef1af314b91ec21e6ed422bc8747ca26
profitsw2000 <profitsw2000@gmail.com>
2024-07-09 22:14:55 +0300

    Добавление кнопок на макет с инженерным калькулятором.

--- scientific/build.gradle.kts
+++ scientific/build.gradle.kts
@@ -1,42 +1,24 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.win7calc.main.lib.gradle.plugin)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
     namespace = "ru.profitsw2000.scientific"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+    implementation(project(":core"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    //ViewModel
+    implementation(libs.androidx.livedata)
+    implementation(libs.androidx.viewmodel)
+    //Koin
+    implementation(libs.koin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
