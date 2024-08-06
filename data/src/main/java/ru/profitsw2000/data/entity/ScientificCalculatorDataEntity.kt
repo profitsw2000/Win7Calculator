8657223289083319e0d42f3cb9c06d37e7737d11
profitsw2000 <profitsw2000@gmail.com>
2024-08-06 11:40:49 +0300

    Создан дата-класс для хранения состояния инженерного калькулятора.

---
+++ data/src/main/java/ru/profitsw2000/data/entity/ScientificCalculatorDataEntity.kt
@@ -0,0 +1,23 @@
+package ru.profitsw2000.data.entity
+
+import ru.profitsw2000.data.constants.NO_ERROR_CODE
+
+data class ScientificCalculatorDataEntity(
+    val mainString: String = "0",
+    val historyString: String = "",
+    val operand: Double = 0.0,
+    val scientificOperationType: ScientificOperationType = ScientificOperationType.NO_OPERATION,
+    val memoryNumber: Double? = null,
+    val errorCode: Int = NO_ERROR_CODE
+)
+
+enum class ScientificOperationType {
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULUS,
+    POWER_Y,
+    Y_ROOT,
+    NO_OPERATION
+}
