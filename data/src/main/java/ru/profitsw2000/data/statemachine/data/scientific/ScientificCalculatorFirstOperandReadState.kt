bff313479acfdf8b47456650b4ff13de5ca7c4d3
profitsw2000 <profitsw2000@gmail.com>
2024-08-06 22:28:41 +0300

    Создан класс-состояние ScientificCalculatorFirstOperandReadState.

--- data/src/main/java/ru/profitsw2000/data/statemachine/data/scientific/ScientificCalculatorFirstOperandReadState.kt
+++ data/src/main/java/ru/profitsw2000/data/statemachine/data/scientific/ScientificCalculatorFirstOperandReadState.kt
@@ -1,93 +1,20 @@
 package ru.profitsw2000.data.statemachine.data.scientific
 
-import ru.profitsw2000.data.entity.GeneralCalculatorDataEntity
-import ru.profitsw2000.data.entity.OperationType
 import ru.profitsw2000.data.entity.ScientificCalculatorDataEntity
 import ru.profitsw2000.data.entity.ScientificOperationType
 import ru.profitsw2000.data.statemachine.action.CalculatorAction
 import ru.profitsw2000.data.statemachine.domain.CalculatorState
-import ru.profitsw2000.data.statemachine.domain.GeneralCalculatorState
 import ru.profitsw2000.data.statemachine.domain.ScientificCalculatorBaseState
-import ru.profitsw2000.data.statemachine.domain.ScientificCalculatorState
 
-class ScientificCalculatorInitialState(
+class ScientificCalculatorFirstOperandReadState(
     override val scientificCalculatorDataEntity: ScientificCalculatorDataEntity
-) : ScientificCalculatorBaseState {
+) : ScientificCalculatorBaseState{
 
     override fun consumeAction(action: CalculatorAction): CalculatorState {
-        return when(action) {
-            CalculatorAction.Add -> TODO()
-            CalculatorAction.AddToMemory -> TODO()
-            CalculatorAction.ArcCosine -> TODO()
-            CalculatorAction.ArcSinus -> TODO()
-            CalculatorAction.ArcTangent -> TODO()
-            CalculatorAction.Backspace -> TODO()
-            CalculatorAction.Clear -> clearAll(scientificCalculatorDataEntity)
-            CalculatorAction.ClearEntered -> TODO()
-            CalculatorAction.ClearMemory -> TODO()
-            CalculatorAction.Cosine -> TODO()
-            CalculatorAction.DecimalDegrees -> TODO()
-            is CalculatorAction.Digit -> TODO()
-            CalculatorAction.Divide -> TODO()
-            CalculatorAction.Dms -> TODO()
-            CalculatorAction.DoublePi -> TODO()
-            CalculatorAction.Equal -> TODO()
-            CalculatorAction.ExponentOfX -> TODO()
-            CalculatorAction.ExponentialForm -> TODO()
-            CalculatorAction.Factorial -> TODO()
-            CalculatorAction.FixedToExponent -> TODO()
-            CalculatorAction.Fraction -> TODO()
-            CalculatorAction.HyperbolicArcCosine -> TODO()
-            CalculatorAction.HyperbolicArcSinus -> TODO()
-            CalculatorAction.HyperbolicArcTangent -> TODO()
-            CalculatorAction.HyperbolicCosine -> TODO()
-            CalculatorAction.HyperbolicSinus -> TODO()
-            CalculatorAction.HyperbolicTangent -> TODO()
-            CalculatorAction.Integer -> TODO()
-            CalculatorAction.Inverse -> TODO()
-            CalculatorAction.LeftBracket -> TODO()
-            CalculatorAction.Logarithm -> TODO()
-            CalculatorAction.Modulus -> TODO()
-            CalculatorAction.Multiply -> TODO()
-            CalculatorAction.NaturalLogarithm -> TODO()
-            CalculatorAction.Percentage -> TODO()
-            CalculatorAction.Pi -> TODO()
-            CalculatorAction.PlusMinus -> TODO()
-            CalculatorAction.ReadMemory -> TODO()
-            CalculatorAction.Recipoc -> TODO()
-            CalculatorAction.RightBracket -> TODO()
-            CalculatorAction.SaveToMemory -> TODO()
-            CalculatorAction.Sinus -> TODO()
-            CalculatorAction.SquareRoot -> TODO()
-            CalculatorAction.SquaredX -> TODO()
-            CalculatorAction.Subtract -> TODO()
-            CalculatorAction.SubtractFromMemory -> TODO()
-            CalculatorAction.Tangent -> TODO()
-            CalculatorAction.TenToPowerOfX -> TODO()
-            CalculatorAction.ThirdRootOfX -> TODO()
-            CalculatorAction.XPowerThree -> TODO()
-            CalculatorAction.XPowerY -> TODO()
-            CalculatorAction.YRootOfX -> TODO()
-        }
+        TODO("Not yet implemented")
     }
-
-    /**
-     * Clear internal memory of calculator
-     * @param generalCalculatorDataEntity - contains current calculator data
-     * @return GeneralCalculatorInitialState with cleared memory of calculator data
-     */
     override fun clearMemory(scientificCalculatorDataEntity: ScientificCalculatorDataEntity): CalculatorState {
-        return ScientificCalculatorInitialState(scientificCalculatorDataEntity.copy(memoryNumber = null))
-    }
-
-    /**
-     * Clears all data, contained in fields mainString, historyString and operand fields of calculator data.
-     * OperationType set to NO_OPERATION state
-     * @param generalCalculatorDataEntity - contains current calculator data
-     * @return Initial state with some fields of calculator data set to default state
-     */
-    override fun clearAll(scientificCalculatorDataEntity: ScientificCalculatorDataEntity): CalculatorState {
-        return ScientificCalculatorInitialState(ScientificCalculatorDataEntity(memoryNumber = scientificCalculatorDataEntity.memoryNumber))
+        TODO("Not yet implemented")
     }
 
     override fun readMemory(scientificCalculatorDataEntity: ScientificCalculatorDataEntity): CalculatorState {
@@ -103,6 +30,10 @@ class ScientificCalculatorInitialState(
     }
 
     override fun subtractNumberFromMemory(scientificCalculatorDataEntity: ScientificCalculatorDataEntity): CalculatorState {
+        TODO("Not yet implemented")
+    }
+
+    override fun clearAll(scientificCalculatorDataEntity: ScientificCalculatorDataEntity): CalculatorState {
         TODO("Not yet implemented")
     }
 
