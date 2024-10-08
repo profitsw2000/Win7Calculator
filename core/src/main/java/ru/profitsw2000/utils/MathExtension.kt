ac2ff1d0687c51842aaedef5d31b6a4b9279d27c
profitsw2000 <profitsw2000@gmail.com>
2024-10-08 15:17:56 +0300

    Начало написания функции вычисления факториала.

--- core/src/main/java/ru/profitsw2000/utils/MathExtension.kt
+++ core/src/main/java/ru/profitsw2000/utils/MathExtension.kt
@@ -3,6 +3,13 @@ package ru.profitsw2000.utils
 import kotlin.math.pow
 import kotlin.math.sinh
 
+private const val sterling_koef_1 = 1.0/12.0
+private const val sterling_koef_2 = 1.0/288.0
+private const val sterling_koef_3 = 139.0/51840.0
+private const val sterling_koef_4 = 571.0/2488320.0
+private const val sterling_koef_5 = 163879.0/209018880.0
+private const val sterling_koef_6 = 5246819.0/75246796800.0
+
 fun Double.calcSinh(): Double {
     val result = sinh(this)
 
@@ -19,9 +26,24 @@ fun Double.powerTo(x: Double): Double {
     return result
 }
 
+fun Double.factorial(): Double {
+    val result = if (this % 1 == 0.0) factorialRecursive(this)
+    else
+
+}
+
 private fun checkForOverflow(double: Double) {
     if (double == Double.POSITIVE_INFINITY ||
         double == Double.NEGATIVE_INFINITY ||
         double == Double.NaN)
         throw ArithmeticException("Overflow of calculated number.")
+}
+
+private fun factorialRecursive(x: Double): Double {
+    return if (x <= 1) 1.0
+    else x* factorialRecursive(x - 1)
+}
+
+private fun gamma(x: Double): Double {
+
 }
