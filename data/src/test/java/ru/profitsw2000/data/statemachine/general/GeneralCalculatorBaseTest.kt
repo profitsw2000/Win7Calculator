401fb66227f441d66d26d2e69d6ffd25cddfd327
profitsw2000 <profitsw2000@gmail.com>
2024-10-30 22:10:42 +0300

    Сделаны тесты для функции doubleToCalculatorString с одним аргументом. Выявлены ошибки, необходима доработка.

--- data/src/test/java/ru/profitsw2000/data/statemachine/general/GeneralCalculatorBaseTest.kt
+++ data/src/test/java/ru/profitsw2000/data/statemachine/general/GeneralCalculatorBaseTest.kt
@@ -6,13 +6,85 @@ import ru.profitsw2000.data.entity.GeneralCalculatorDataEntity
 import ru.profitsw2000.data.statemachine.data.general.GeneralCalculatorInitialState
 
 class GeneralCalculatorBaseTest {
+
     @Test
     fun simpleNumber() {
         assertEquals(
-            "1,2345678901234568e+22",
+            "123,45",
             GeneralCalculatorInitialState(
                 GeneralCalculatorDataEntity()
-            ).doubleToCalculatorString(12345678901234567890123.0)
+            ).doubleToCalculatorString(123.45)
         )
     }
+
+    @Test
+    fun overSixtyDecimal() {
+        assertEquals(
+            "0,123456789012346",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(0.123456789012345678)
+        )
+    }
+
+    @Test
+    fun fourIntSixtyDecimal() {
+        assertEquals(
+            "1234,123456789012",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(1234.1234567890123456)
+        )
+    }
+
+    @Test
+    fun negativeNumber() {
+        assertEquals(
+            "-1234,56789",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(-1234.567890)
+        )
+    }
+
+    @Test
+    fun veryBigPositiveNumber() {
+        assertEquals(
+            "1e+16",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(10000000000000000.0)
+        )
+    }
+
+    @Test
+    fun veryBigNegativeNumber() {
+        assertEquals(
+            "-1e+16",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(-10000000000000000.0)
+        )
+    }
+
+    @Test
+    fun verySmallPositiveNumber() {
+        assertEquals(
+            "9e-16",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(0.00000000000000009)
+        )
+    }
+
+    @Test
+    fun verySmallNegativeNumber() {
+        assertEquals(
+            "-9e-16",
+            GeneralCalculatorInitialState(
+                GeneralCalculatorDataEntity()
+            ).doubleToCalculatorString(-0.00000000000000009)
+        )
+    }
+
 }
